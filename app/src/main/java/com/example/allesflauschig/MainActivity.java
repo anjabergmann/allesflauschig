e2034e185d2b367f1ab431d74a564d5d075ea605
anjab <anja.bergmann@hotmail.com>
2024-01-03 19:46:15 +0100

    Move notification bulider to NotificationUtils to clean MainActivity

--- app/src/main/java/com/example/allesflauschig/MainActivity.java
+++ app/src/main/java/com/example/allesflauschig/MainActivity.java
@@ -1,13 +1,7 @@
 package com.example.allesflauschig;
 
-
-import static com.example.allesflauschig.utils.AllesFlauschigConstants.KEY_TEXT_REPLY;
-import static com.example.allesflauschig.utils.AllesFlauschigConstants.NOTIFICATION_CHANNEL_ID;
-
 import android.Manifest;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -15,11 +9,8 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.RemoteInput;
 
 import com.example.allesflauschig.Exception.CrashHandler;
-import com.example.allesflauschig.utils.AllesFlauschigConstants.Extras;
 import com.example.allesflauschig.utils.NotificationUtils;
 
 import java.util.logging.Logger;
@@ -53,66 +44,10 @@ public class MainActivity extends AppCompatActivity {
         this.startActivity(myIntent);
     }
 
-    private void createNotificationChannel() {
-        CharSequence name = getString(R.string.channel_name);
-        String description = getString(R.string.channel_description);
-        int importance = NotificationManager.IMPORTANCE_DEFAULT;
-        NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
-        channel.setDescription(description);
-        // Register the channel with the system; you can't change the importance
-        // or other notification behaviors after this.
-        NotificationManager notificationManager = getSystemService(NotificationManager.class);
-        notificationManager.createNotificationChannel(channel);
-    }
-
     private void createNotification() {
-
-        CharSequence[] options = { "-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5" };
-
-        String replyLabel = "Wie geht's? (-5 mies, 0 mittel, 5 gut)";
-        RemoteInput remoteInput = new RemoteInput.Builder(KEY_TEXT_REPLY)
-                .setLabel(replyLabel)
-                .setAllowFreeFormInput(true)
-                //.setChoices(options)
-                .build();
-
-        Intent someIntent = new Intent(this, MyReceiver.class);
-        someIntent.setAction("com.example.broadcast.MY_NOTIFICATION");
-        //someIntent.putExtra("data", "AAAAAAAANothing to see here, move along.");
-        someIntent.putExtra("blub", "AAAAAAAANothing to see here, move along.");
-        someIntent.putExtra(Extras.NOTIFICATION_ID, "13");
-
-        //sendBroadcast(someIntent);
-
-        // Build a PendingIntent for the reply action to trigger.
-        PendingIntent replyPendingIntent =
-                PendingIntent.getBroadcast(getApplicationContext(),
-                        13, someIntent, PendingIntent.FLAG_MUTABLE);
-
-        // Create the reply action and add the remote input.
-        NotificationCompat.Action action =
-                new NotificationCompat.Action.Builder(R.drawable.ic_stat_onesignal_default,
-                        "Wie geht's?", replyPendingIntent)
-                        .addRemoteInput(remoteInput)
-                        .build();
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), NOTIFICATION_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_stat_onesignal_default)
-                .setContentTitle("Alles flauschig?")
-                //.setContentText("Much longer text that cannot fit one line...")
-                .setStyle(new NotificationCompat.DecoratedCustomViewStyle()
-                        //.bigText("Much longer text that cannot fit one line... lalalala ich bin ein Keks jajajaja Kekse Kekske Kekse yummie yum"))
-                )
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(replyPendingIntent)
-                //.setOngoing(true)
-                .setAutoCancel(true)
-                .addAction(action);
-
-
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.notify(13, builder.build());
+            notificationManager.notify(13, NotificationUtils.createNotification(this));
         } else {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
         }
--- app/src/main/java/com/example/allesflauschig/utils/NotificationUtils.java
+++ app/src/main/java/com/example/allesflauschig/utils/NotificationUtils.java
@@ -1,11 +1,19 @@
 package com.example.allesflauschig.utils;
 
+import static com.example.allesflauschig.utils.AllesFlauschigConstants.KEY_TEXT_REPLY;
 import static com.example.allesflauschig.utils.AllesFlauschigConstants.NOTIFICATION_CHANNEL_ID;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.RemoteInput;
+
+import com.example.allesflauschig.MyReceiver;
 import com.example.allesflauschig.R;
 
 public class NotificationUtils {
@@ -20,6 +28,53 @@ public class NotificationUtils {
         // or other notification behaviors after this.
         NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
         notificationManager.createNotificationChannel(channel);
+    }
+
+    public static Notification createNotification(Context context) {
+
+        CharSequence[] options = { "-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5" };
+
+        String replyLabel = "Wie geht's? (-5 mies, 0 mittel, 5 gut)";
+        RemoteInput remoteInput = new RemoteInput.Builder(KEY_TEXT_REPLY)
+                .setLabel(replyLabel)
+                .setAllowFreeFormInput(true)
+                //.setChoices(options)
+                .build();
+
+        Intent someIntent = new Intent(context, MyReceiver.class);
+        someIntent.setAction("com.example.broadcast.MY_NOTIFICATION");
+        //someIntent.putExtra("data", "AAAAAAAANothing to see here, move along.");
+        someIntent.putExtra("blub", "AAAAAAAANothing to see here, move along.");
+        someIntent.putExtra(AllesFlauschigConstants.Extras.NOTIFICATION_ID, "13");
+
+        //sendBroadcast(someIntent);
+
+        // Build a PendingIntent for the reply action to trigger.
+        PendingIntent replyPendingIntent =
+                PendingIntent.getBroadcast(context.getApplicationContext(),
+                        13, someIntent, PendingIntent.FLAG_MUTABLE);
+
+        // Create the reply action and add the remote input.
+        NotificationCompat.Action action =
+                new NotificationCompat.Action.Builder(R.drawable.ic_stat_onesignal_default,
+                        "Wie geht's?", replyPendingIntent)
+                        .addRemoteInput(remoteInput)
+                        .build();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context.getApplicationContext(), NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_onesignal_default)
+                .setContentTitle("Alles flauschig?")
+                //.setContentText("Much longer text that cannot fit one line...")
+                .setStyle(new NotificationCompat.DecoratedCustomViewStyle()
+                        //.bigText("Much longer text that cannot fit one line... lalalala ich bin ein Keks jajajaja Kekse Kekske Kekse yummie yum"))
+                )
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(replyPendingIntent)
+                //.setOngoing(true)
+                .setAutoCancel(true)
+                .addAction(action);
+
+        return builder.build();
     }
 
 }
